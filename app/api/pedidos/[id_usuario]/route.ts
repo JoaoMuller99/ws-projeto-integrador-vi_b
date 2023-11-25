2b03620e3d9b8490c36cbd4b4eeab751f88c42d1
Joao <jp99muller@gmail.com>
2023-11-25 17:07:57 -0300

    fix: adição pratos endpoint pedidos

--- app/api/pedidos/[id_usuario]/route.ts
+++ app/api/pedidos/[id_usuario]/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest, context: { params: { id_usuario: number } }) {
   try {
     const idUsuario = +context.params.id_usuario;
-    return NextResponse.json(await prisma.pedidos.findMany({ where: { id_usuario: idUsuario } }));
+    return NextResponse.json(await prisma.pedidos.findMany({ where: { id_usuario: idUsuario }, include: { prato: true } }));
   } catch (e) {
     return NextResponse.json({ error: String(e) || "An error ocurred while trying to filter the orders" }, { status: 404 });
   }
