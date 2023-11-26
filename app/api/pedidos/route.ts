2555f1e896ee95afcad292e82e45539b291f4d4d
Joao <jp99muller@gmail.com>
2023-11-26 13:35:06 -0300

    fix: correção endpoint pedidos

--- app/api/pedidos/route.ts
+++ app/api/pedidos/route.ts
@@ -1,26 +1,25 @@
+import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   try {
     const data = await request.json();
 
-    return NextResponse.json(data.pratos);
+    const newOrder = await prisma.pedidos.create({
+      data: {
+        id_usuario: data.id_usuario,
+        id_estabelecimento: data.id_estabelecimento,
+        tipo_pagamento: data.tipo_pagamento,
+        comentarios: data.comentarios,
+        valor: data.valor,
+        status: data.status,
+        prato: {
+          create: data.pratos.map((item: any) => ({ prato: { connect: { id: item.id } } })),
+        },
+      },
+    });
 
-    // const newOrder = await prisma.pedidos.create({
-    //   data: {
-    //     id_usuario: data.id_usuario,
-    //     id_estabelecimento: data.id_estabelecimento,
-    //     tipo_pagamento: data.tipo_pagamento,
-    //     comentarios: data.comentarios,
-    //     valor: data.valor,
-    //     status: data.status,
-    //     prato: {
-    //       create: data.pratos.map((prato: number) => ({ prato: { connect: { id: prato } } })),
-    //     },
-    //   },
-    // });
-
-    // return NextResponse.json(newOrder);
+    return NextResponse.json(newOrder);
   } catch (e) {
     return NextResponse.json({ error: String(e) || "An error ocurred while trying to create a order" }, { status: 404 });
   }
